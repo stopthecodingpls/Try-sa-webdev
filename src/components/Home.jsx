43b7f1b9268b83ff626c7929da699b3bb16c0e3c
stopthecodingpls <selwynvalera18@gmail.com>
2024-12-18 21:26:41 +0800

    updated Home and Navbar jsx and css, and new pic

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ const HomeSection = () => {
     <section className="home-section">
       <h1>Welcome to Tasty</h1>
       <p>Home of Food Enthusiasts and Home Cooks</p>
-      <img className="chef-image" src="/assets/chef.svg"></img>
+      <img className="chef-image" src="/assets/images/chef.svg"></img>
     </section>
   );
 };
