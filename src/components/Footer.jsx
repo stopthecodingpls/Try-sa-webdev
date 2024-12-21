1e1fd3f9ffb39f528731b5e8271c0c9060c513a6
stopthecodingpls <selwynvalera18@gmail.com>
2024-12-21 22:01:05 +0800

    New Home page and Footer. File path issue resolved

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Css/Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-bottom">
+        <p>Tasty</p>
+        <p>Delivering quality and excellence</p>
+        <p>&copy; 2024 Company Name. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import Navbar from "./Navbar";
-import "./Css/Home.css";
 import FormImage from "../assets/images/chef.svg";
+import "./Css/Home.css";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
 
 const HomeSection = () => {
   return (
@@ -10,8 +11,32 @@ const HomeSection = () => {
       <section className="home-section">
         <h1 className="font-logo">Welcome to Tasty</h1>
         <p className="font-logo">Home of Food Enthusiasts and Home Cooks</p>
-        <img className="chef-image" src={FormImage} style={{ display: "block", margin: "0 auto" }}></img>
+        <div className="bordered-section">
+          <img className="chef-image" src={FormImage} alt="Chef illustration" style={{ display: "block", margin: "0 auto" }}/>
+          <p>
+            Explore a world of culinary inspiration with our various collection
+            of recipes suitable for any occasion. We provide step-by-step
+            instructions for quick and easy meals as well as stunning dishes for
+            special occasions. Discover key cooking methods, how to choose the
+            freshest ingredients, and how to add unique twists to classic
+            dishes. Whether you're a seasoned chef or a first-time home cook,
+            our recipes are meant to inspire you and bring joy to your table.
+          </p>
+        </div>
+        <div className="bordered-section">
+          <p>
+            Explore a world of culinary inspiration with our various collection
+            of recipes suitable for any occasion. We provide step-by-step
+            instructions for quick and easy meals as well as stunning dishes for
+            special occasions. Discover key cooking methods, how to choose the
+            freshest ingredients, and how to add unique twists to classic
+            dishes. Whether you're a seasoned chef or a first-time home cook,
+            our recipes are meant to inspire you and bring joy to your table.
+          </p>
+          <img className="chef-image" src={FormImage} alt="Chef illustration" style={{ display: "block", margin: "0 auto" }}/>
+        </div>
       </section>
+      <Footer />
     </div>
   );
 };
